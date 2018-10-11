d4cdce796fe73168baaf3642f4d444bb69d6f521
Emerson-Torres <30862610+Emerson-Torres@users.noreply.github.com>
2018-10-11 14:44:18 -0600

    activar voto completo

--- src/java/sv/edu/udb/www/managed_beans/PresidentejrvBean.java
+++ src/java/sv/edu/udb/www/managed_beans/PresidentejrvBean.java
@@ -4,13 +4,21 @@
  * and open the template in the editor.
  */
 package sv.edu.udb.www.managed_beans;
-
+import java.util.Calendar;
 import java.util.List;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import javax.servlet.http.HttpServletRequest;
+import sv.edu.udb.www.entities.CentrovotacionEntity;
 import sv.edu.udb.www.entities.EleccionesEntity;
+import sv.edu.udb.www.entities.JrvEntity;
 import sv.edu.udb.www.model.PresidentejrvModel;
 import sv.edu.udb.www.utils.JsfUtils;
 
@@ -25,19 +33,48 @@ public class PresidentejrvBean {
     @EJB
     private PresidentejrvModel presidentejrvModel;
 
-    
     HttpServletRequest request = JsfUtils.getRequest();
     EleccionesEntity eleccion = new EleccionesEntity();
-    
+    JrvEntity jrv = new JrvEntity();
+
     public PresidentejrvBean() {
-        request.getSession().setAttribute("valor",1);
+        request.getSession().setAttribute("valor", 1);
     }
 
-    
-    
-    public List<EleccionesEntity> getObtenerEleccion(){
-    
-    return presidentejrvModel.listaEleccionPresidente(1);
+    public List<EleccionesEntity> getObtenerEleccion() {
+        return presidentejrvModel.listaEleccionPresidente((int) request.getSession().getAttribute("valor"));
     }
     
+    public String nombrePresidente(){
+    return presidentejrvModel.nombrePresidente((int) request.getSession().getAttribute("valor"));
+    }
+
+    public List<CentrovotacionEntity> getListaCentro() {
+        return presidentejrvModel.obtenerCentroPresidente((int) request.getSession().getAttribute("valor"));
+    }
+
+    public String activarVotacion() {
+        try {
+            Calendar c = Calendar.getInstance();
+            String horas, minutos, segundos, hora;
+            horas = Integer.toString(c.get(Calendar.HOUR_OF_DAY));
+            minutos = Integer.toString(c.get(Calendar.MINUTE));
+            segundos = Integer.toString(c.get(Calendar.SECOND));
+            hora = horas + ":" + minutos + ":" + segundos;
+            jrv = presidentejrvModel.obtenerJrv((int) request.getSession().getAttribute("valor"));
+            DateFormat hora2 = new SimpleDateFormat("HH:mm:ss");
+            Date convertido = hora2.parse(hora);
+            jrv.setHoraApertura(convertido);
+            if(presidentejrvModel.activarJrv(jrv)==0){
+            return null;
+            }else{
+            return "";
+            }                      
+        } catch (ParseException ex) {
+            Logger.getLogger(PresidentejrvBean.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+      
+    }
+
 }
--- src/java/sv/edu/udb/www/model/PresidentejrvModel.java
+++ src/java/sv/edu/udb/www/model/PresidentejrvModel.java
@@ -1,4 +1,3 @@
-
 package sv.edu.udb.www.model;
 
 import java.util.List;
@@ -6,7 +5,9 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import sv.edu.udb.www.entities.CentrovotacionEntity;
 import sv.edu.udb.www.entities.EleccionesEntity;
+import sv.edu.udb.www.entities.JrvEntity;
 
 @Stateless
 public class PresidentejrvModel {
@@ -14,12 +15,45 @@ public class PresidentejrvModel {
     @PersistenceContext(unitName = "ProyectoPooPeriodo2PU")
     private EntityManager em;
 
-    public List<EleccionesEntity> listaEleccionPresidente(int valor){
-        System.out.println("Ingreso");
-    Query query = em.createNativeQuery("SELECT e.* FROM miembrojrv m INNER JOIN jrv j ON m.IdJrv = j.IdJrv INNER JOIN elecciones e on j.IdEleccion = e.IdEleccion WHERE m.IdMiembroJrv = ?", EleccionesEntity.class);
-    query.setParameter(1, valor);
-        
-    return query.getResultList();
+    public String nombrePresidente(int idvalor){
+    Query query = em.createNativeQuery("SELECT CONCAT(SUBSTRING(c.NombresCiudadano,1,LOCATE(' ', c.NombresCiudadano)) , SUBSTRING(c.ApellidosCiudadano,1,LOCATE(' ', c.ApellidosCiudadano)) ) as nombre FROM miembrojrv j INNER JOIN ciudadanos c on j.DuiCiudadano = c.DuiCiudadano WHERE j.IdMiembroJrv =?");
+    query.setParameter(1, idvalor);
+    Object valor = query.getSingleResult();
+    return (String) valor;
     }
     
+    public List<EleccionesEntity> listaEleccionPresidente(int valor) {
+       
+        Query query = em.createNativeQuery("SELECT e.* FROM miembrojrv m INNER JOIN jrv j ON m.IdJrv = j.IdJrv INNER JOIN elecciones e on j.IdEleccion = e.IdEleccion WHERE m.IdMiembroJrv = ?", EleccionesEntity.class);
+        query.setParameter(1, valor);
+        return query.getResultList();
+    }
+
+    public List<CentrovotacionEntity> obtenerCentroPresidente(int valor) {
+        Query query = em.createNativeQuery("SELECT c.* FROM miembrojrv m INNER JOIN jrv j on m.IdJrv = j.IdJrv INNER JOIN centrovotacion c on j.IdCentroVotacion=c.IdCentroVotacion WHERE m.IdMiembroJrv = ?",CentrovotacionEntity.class);
+        query.setParameter(1, valor);
+        return query.getResultList();
+    }
+    
+    public JrvEntity obtenerJrv(int idvalor){
+        try{
+        Integer valor2;
+        Query query = em.createNativeQuery("SELECT IdJrv FROM miembrojrv WHERE IdMiembroJrv = ?");
+        query.setParameter(1, idvalor);
+        Object valor = query.getSingleResult();
+        valor2 = (Integer) valor;
+        return em.find(JrvEntity.class, valor2);
+        }catch(Exception e){
+            return null;
+        }
+    }
+    public int activarJrv(JrvEntity jrv){
+     try {
+            em.merge(jrv);
+            em.flush();
+            return 1;
+        } catch (Exception e) {
+            return 0;
+        }
+    }
 }
