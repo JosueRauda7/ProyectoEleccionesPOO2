0ebaaeac9eb68f35c11e09e193f617b6bdfc99a3
Emerson-Torres <30862610+Emerson-Torres@users.noreply.github.com>
2018-10-10 20:14:48 -0600

    error

--- src/java/sv/edu/udb/www/managed_beans/PresidentejrvBean.java
+++ src/java/sv/edu/udb/www/managed_beans/PresidentejrvBean.java
@@ -5,8 +5,14 @@
  */
 package sv.edu.udb.www.managed_beans;
 
+import java.util.List;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.servlet.http.HttpServletRequest;
+import sv.edu.udb.www.entities.EleccionesEntity;
+import sv.edu.udb.www.model.PresidentejrvModel;
+import sv.edu.udb.www.utils.JsfUtils;
 
 /**
  *
@@ -16,9 +22,22 @@ import javax.enterprise.context.RequestScoped;
 @RequestScoped
 public class PresidentejrvBean {
 
-   
-    public PresidentejrvBean() {
-    }
+    @EJB
+    private PresidentejrvModel presidentejrvModel;
+
     
+    HttpServletRequest request = JsfUtils.getRequest();
+    EleccionesEntity eleccion = new EleccionesEntity();
+    
+    public PresidentejrvBean() {
+        request.getSession().setAttribute("valor",1);
+    }
+
+    
+    
+    public List<EleccionesEntity> getObtenerEleccion(){
+    
+    return presidentejrvModel.listaEleccionPresidente(1);
+    }
     
 }
--- src/java/sv/edu/udb/www/model/PresidentejrvModel.java
+++ src/java/sv/edu/udb/www/model/PresidentejrvModel.java
@@ -1,19 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sv.edu.udb.www.model;
 
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import sv.edu.udb.www.entities.EleccionesEntity;
 
-/**
- *
- * @author Emerson Torres
- */
 @Stateless
 public class PresidentejrvModel {
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    @PersistenceContext(unitName = "ProyectoPooPeriodo2PU")
+    private EntityManager em;
+
+    public List<EleccionesEntity> listaEleccionPresidente(int valor){
+    Query query = em.createNativeQuery("SELECT e.* FROM miembrojrv m INNER JOIN jrv j ON m.IdJrv = j.IdJrv INNER JOIN elecciones e on j.IdEleccion = e.IdEleccion WHERE m.IdMiembroJrv = :valor");
+    query.setParameter("valor", valor);
+    return query.getResultList();
+    }
+    
 }
