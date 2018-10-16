9997c0ad54a69b376b743fe9180d0e00f0f23b32
VlRoque <roquealexander7@gmail.com>
2018-10-16 13:13:24 -0600

    Progreso

--- src/java/sv/edu/udb/www/managed_beans/CiudadanosBean.java
+++ src/java/sv/edu/udb/www/managed_beans/CiudadanosBean.java
@@ -4,6 +4,8 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 import sv.edu.udb.www.entities.CiudadanosEntity;
 import sv.edu.udb.www.model.CiudadanosModel;
 import sv.edu.udb.www.utils.JsfUtils;
@@ -14,29 +16,11 @@ public class CiudadanosBean {
 
     @EJB
     private CiudadanosModel ciudadanosModel;
-    
-    List<CiudadanosEntity> listaCiudadanos;
+
+    //List<CiudadanosEntity> listaCiudadanos;
     private CiudadanosEntity ciudadanos = new CiudadanosEntity();
-    
-    
+
     public CiudadanosBean() {
-    }
-
-
-    //Métodos
-    
-    public List<CiudadanosEntity> getListaCiudadanos(){
-        listaCiudadanos = ciudadanosModel.listarCiudadanos();
-        return listaCiudadanos;
-    }
-    
-    public String ingresarCiudadano(){
-        if(ciudadanosModel.ingresarCiudadanos(ciudadanos) == 0){
-                        JsfUtils.addErrorMessage("idDepartamento", "Ya existe ese departamento.");
-            return null;
-        }
-        JsfUtils.addFlashMessage("exito", "Ciudadano insertado exitosamente");
-        return "/gestionVotantes/ingresarCiudadano?faces-redirect=true";
     }
     
     public CiudadanosEntity getCiudadanos() {
@@ -46,5 +30,49 @@ public class CiudadanosBean {
     public void setCiudadanos(CiudadanosEntity ciudadanos) {
         this.ciudadanos = ciudadanos;
     }
-    
+
+    //Métodos
+    public List<CiudadanosEntity> getListaCiudadanos() {
+        return ciudadanosModel.listarCiudadanos();
+    }
+
+    public String ingresarCiudadano() {
+        if (ciudadanosModel.ingresarCiudadanos(ciudadanos) == 0) {
+            JsfUtils.addErrorMessage("fracaso", "Ya existe ese departamento.");
+            return null;
+        }
+        JsfUtils.addFlashMessage("exito", "Ciudadano insertado exitosamente");
+        return "/gestionVotantes/ingresarCiudadano?faces-redirect=true";
+    }
+
+    public String obtenerCiudadano() {
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        String codigo = request.getParameter("codigo");
+        ciudadanos = ciudadanosModel.obtenerCiudadano(codigo);
+
+        return "/gestionVotantes/modificarCiudadanos";
+    }
+
+    public String modificarCiudadano() {
+        if (ciudadanosModel.modificarCiudadanos(ciudadanos) == 0) {
+            JsfUtils.addErrorMessage("fracaso", "No se pudo modificar");
+            return null;
+        }
+
+        JsfUtils.addFlashMessage("exito", "El registro del ciudadano se ha modificado");
+        return "/gestionVotantes/listaEditoriales?faces-redirect=true";
+    }
+
+    public String eliminarCiudadano() {
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        String codigo = request.getParameter("codigo");
+
+        if (ciudadanosModel.eliminarCiudadanos(codigo) > 0) {
+            JsfUtils.addFlashMessage("exito", "El registro del ciudadano se ha eliminado");
+        } else {
+            JsfUtils.addFlashMessage("fracaso", "No se puede eliminar el registro de este ciudadano");
+        }
+        return null;
+    }
+
 }
--- src/java/sv/edu/udb/www/model/CiudadanosModel.java
+++ src/java/sv/edu/udb/www/model/CiudadanosModel.java
@@ -13,17 +13,46 @@ public class CiudadanosModel {
     @PersistenceContext(unitName = "ProyectoPooPeriodo2PU")
     private EntityManager em;
 
-    public List<CiudadanosEntity> listarCiudadanos(){
+    public List<CiudadanosEntity> listarCiudadanos() {
         Query query = em.createNamedQuery("CiudadanosEntity.findAll");
         return query.getResultList();
     }
 
-    public int ingresarCiudadanos(CiudadanosEntity ciudadanos){
-        try{
+    public int ingresarCiudadanos(CiudadanosEntity ciudadanos) {
+        try {
             em.persist(ciudadanos);
             em.flush();
             return 1;
-        }catch(Exception e){
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    public CiudadanosEntity obtenerCiudadano(String codigo) {
+        return em.find(CiudadanosEntity.class, codigo);
+    }
+
+    public int modificarCiudadanos(CiudadanosEntity ciudadano) {
+        try {
+            em.merge(ciudadano);
+            em.flush();
+            return 1;
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    public int eliminarCiudadanos(String codigo) {
+        try {
+            CiudadanosEntity ciudadano = em.find(CiudadanosEntity.class, codigo);
+
+            if (ciudadano != null) {
+                em.remove(ciudadano);
+                em.flush();
+                return 1;
+            }
+            return 0;
+        } catch (Exception e) {
             return 0;
         }
     }
