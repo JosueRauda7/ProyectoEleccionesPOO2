f15c4b89c96b5893dc4d041c0d04b1f107776604
JosueRauda7 <30637512+JosueRauda7@users.noreply.github.com>
2018-10-17 21:49:27 -0600

    generador de jrvs

--- src/java/sv/edu/udb/www/managed_beans/EleccionesBean.java
+++ src/java/sv/edu/udb/www/managed_beans/EleccionesBean.java
@@ -1,12 +1,20 @@
 package sv.edu.udb.www.managed_beans;
 
+import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.servlet.http.HttpServletRequest;
+import sv.edu.udb.www.entities.CentrovotacionEntity;
+import sv.edu.udb.www.entities.CiudadanosEntity;
 import sv.edu.udb.www.entities.EleccionesEntity;
+import sv.edu.udb.www.entities.JrvEntity;
 import sv.edu.udb.www.entities.TipoeleccionesEntity;
+import sv.edu.udb.www.model.CentrosVotacionesModel;
+import sv.edu.udb.www.model.CiudadanosModel;
 import sv.edu.udb.www.model.EleccionesModel;
+import sv.edu.udb.www.model.JrvModel;
 import sv.edu.udb.www.utils.JsfUtils;
 
 @Named(value = "eleccionesBean")
@@ -14,7 +22,18 @@ import sv.edu.udb.www.utils.JsfUtils;
 public class EleccionesBean {
 
     @EJB
+    private CentrosVotacionesModel centrosVotacionesModel;
+
+    @EJB
+    private CiudadanosModel ciudadanosModel;
+
+    @EJB
+    private JrvModel jrvModel;
+
+    @EJB
     private EleccionesModel eleccionesModel;
+
+    private JrvEntity jrv = new JrvEntity();
 
     private EleccionesEntity eleccion = new EleccionesEntity();
 
@@ -22,7 +41,17 @@ public class EleccionesBean {
 
     private List<EleccionesEntity> listaElecciones;
 
+    private Date fechaActual = new Date();
+
     public EleccionesBean() {
+    }
+
+    public JrvEntity getJrv() {
+        return jrv;
+    }
+
+    public void setJrv(JrvEntity jrv) {
+        this.jrv = jrv;
     }
 
     public EleccionesEntity getEleccion() {
@@ -36,6 +65,14 @@ public class EleccionesBean {
     public List<TipoeleccionesEntity> getListaTiposElecciones() {
         listaTiposElecciones = eleccionesModel.listarTipoElecciones();
         return listaTiposElecciones;
+    }
+
+    public Date getFechaActual() {
+        return fechaActual;
+    }
+
+    public void setFechaActual(Date fechaActual) {
+        this.fechaActual = fechaActual;
     }
 
     public void setListaTiposElecciones(List<TipoeleccionesEntity> listaTiposElecciones) {
@@ -52,6 +89,19 @@ public class EleccionesBean {
     }
 
     //metodos
+    public String generarJRVs() {
+        HttpServletRequest request = JsfUtils.getRequest();
+        String id = request.getParameter("codigo");
+        List<CentrovotacionEntity> listaCentroVotacion = centrosVotacionesModel.listarCentrosVotacion();
+        eleccion = eleccionesModel.obtenerEleccion(id);
+        if (jrvModel.generarJRVs(eleccion, listaCentroVotacion) == 0) {
+            JsfUtils.addFlashMessage("fracaso", "JRVs no creados exitosamente.");
+            return "/administradorGeneral/listaElecciones?faces-redirect=true";
+        }
+        JsfUtils.addFlashMessage("exito", "JRVs creados exitosamente.");
+        return "/administradorGeneral/listaElecciones?faces-redirect=true";
+    }
+
     public String guardarEleccion() {
         if (eleccionesModel.insertarEleccion(eleccion) == 0) {
             JsfUtils.addErrorMessage("idEleccion", "Ya existe un ID con esa elección.");
--- src/java/sv/edu/udb/www/model/JrvModel.java
+++ src/java/sv/edu/udb/www/model/JrvModel.java
@@ -10,8 +10,11 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import sv.edu.udb.www.entities.CentrovotacionEntity;
+import sv.edu.udb.www.entities.CiudadanojrvEntity;
+import sv.edu.udb.www.entities.CiudadanosEntity;
+import sv.edu.udb.www.entities.EleccionesEntity;
 import sv.edu.udb.www.entities.JrvEntity;
-import sv.edu.udb.www.entities.MiembrojrvEntity;
 
 /**
  *
@@ -23,32 +26,81 @@ public class JrvModel {
     @PersistenceContext(unitName = "ProyectoPooPeriodo2PU")
     private EntityManager em;
 
-    public List<JrvEntity> listarJRVs(){
+    public List<JrvEntity> listarJRVs() {
         Query query = em.createNamedQuery("JrvEntity.findAll");
         return query.getResultList();
     }
-    
-    public int insertarJRV(JrvEntity jrv){
-        try{
+
+    public int insertarJRV(JrvEntity jrv) {
+        try {
             em.persist(jrv);
             em.flush();
             return 1;
-        }catch(Exception e){
+        } catch (Exception e) {
             return 0;
         }
     }
-    
-    public JrvEntity obtenerJRV(int id){
+
+    public JrvEntity obtenerJRV(int id) {
         return em.find(JrvEntity.class, id);
     }
-    
-    public int asignarPresidenteJRV(JrvEntity presidente){
-        try{
+
+    public int asignarPresidenteJRV(JrvEntity presidente) {
+        try {
             em.merge(presidente);
             em.flush();
             return 1;
-        }catch(Exception e){
+        } catch (Exception e) {
             return 0;
         }
+    }
+
+    public JrvEntity obtenerJRVporCentroVotacion(CentrovotacionEntity centroVotacion) {
+        Query query = em.createQuery("select j from JrvEntity j where j.idCentroVotacion.idCentroVotacion = ?1");
+        query.setParameter(1,centroVotacion.getIdCentroVotacion());
+        return (JrvEntity) query.getSingleResult();
+    }
+
+    public int generarJRVs(EleccionesEntity eleccion, List<CentrovotacionEntity> listaCentroVotacion) {
+        
+            int cantidadCiudadanos = 0;
+            double cantidadJRVs = 0;
+            for (int i = 0; i <= listaCentroVotacion.size(); i++) {
+                List<CiudadanosEntity> listaCiudadanos;
+                JrvEntity jrv = new JrvEntity();
+                
+                CentrovotacionEntity centroVotacion = new CentrovotacionEntity();
+                centroVotacion = listaCentroVotacion.get(i);
+
+                jrv.setIdCentroVotacion(centroVotacion);
+                jrv.setIdEleccion(eleccion);
+                jrv.setEstadoFinalizado((Integer) 0);
+
+                listaCiudadanos = centroVotacion.getCiudadanosEntityList();
+                cantidadCiudadanos = listaCiudadanos.size();
+                //Math.ceil para redondear todos los decimales a su valor entero mas proximo
+                cantidadJRVs = Math.ceil((double)cantidadCiudadanos /(double) 200);
+                for (int j = 1; j <= cantidadJRVs; j++) {
+                    //insertar jrv
+                    em.persist(jrv);
+                    em.flush();
+                    jrv = this.obtenerJRVporCentroVotacion(centroVotacion);
+                    for (int k = (200 * (j - 1)); (k < (200 * j)); k++) {
+                        CiudadanosEntity ciudadano = new CiudadanosEntity();
+                        CiudadanojrvEntity ciudadanoJRV = new CiudadanojrvEntity();
+                        if(k==cantidadCiudadanos){
+                            k=(200 * j);
+                            return 1;
+                        }
+                        ciudadano = listaCiudadanos.get(k);
+                        ciudadanoJRV.setDuiCiudadano(ciudadano);
+                        ciudadanoJRV.setIdJrv(jrv);
+                        em.persist(ciudadanoJRV);
+                        em.flush();
+                    }
+                }
+            }
+            return 1;
+        
     }
 }
